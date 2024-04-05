10f8d3ee0246871263d2d7a9da22ac0455804da4
Ucmessina <monre036@gmail.com>
2024-04-05 20:23:03 +0200

    changes 1

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const buttonElement = document.getElementById('button-menu');
+const menuElement = document.getElementById('menu');
+
+const handleMenu = () => {
+  if (menuElement.classList.contains('show')) {
+    menuElement.classList.remove('show');
+  } else {
+    menuElement.classList.add('show');
+  }
+};
+
+buttonElement.addEventListener('click', handleMenu);
